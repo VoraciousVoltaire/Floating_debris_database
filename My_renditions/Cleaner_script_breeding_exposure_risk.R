5d3ec81d02347eb1e8d525cdc23b3906f19e8303
Azure._.Damsel <amey19@iiserb.ac.in>
2023-12-19 14:43:52 +0100

    Some modifications in clean nbs script and an overarching loop for extracting 95% UDs for multiple colonies

---
+++ My_renditions/Cleaner_script_breeding_exposure_risk.R
@@ -0,0 +1,285 @@
+# Loading in essential packages ----
+library(sp)
+library(raster)
+library(dplyr)
+library(RColorBrewer)
+library(sf)
+library(spData)
+library(googlesheets4)
+library(ggplot2)
+library(ggpubr)
+library(gridExtra)
+library(terra)
+
+# Setting up the right directory ----
+setwd('/Users/ameydanole/Desktop/ENS_Rennes/argh/Microplastic_ingestion_by_fulmarus_glacialis/1_full_analysis_petrels/')
+
+# Data to read in ----
+Lebreton <- as.matrix(read.csv("input_data/plastics_data/lebretonmodel_abundance.csv", header = F))
+Maximenko <- as.matrix(read.csv("input_data/plastics_data/maximenkomodel_abundance.csv", header = F))
+VanSeb <- as.matrix(read.csv("input_data/plastics_data/vansebillemodel_abundance.csv", header = F))
+
+# Data Cleanup ----
+df <- data.frame(van = as.vector(VanSeb), max = as.vector(Maximenko), leb = as.vector(Lebreton))
+
+# Geometric mean ----
+Average <- rowMeans(mutate_all(df, function(x) log10(x+1)) ,na.rm = T)
+
+dim(Average) <- c(181, 361)
+
+Ave <- raster(Average, xmn = 1, xmx= 361, ymn=-90, ymx=90, 
+              crs="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+plot(Ave)
+
+AveRaster_2 <- raster :: rotate(raster(Average, 
+                                       xmn = 0, xmx= 360, ymn=-90, ymx=90, 
+                                       crs="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+plot(AveRaster_2)
+
+# After rotating to atlantic-centred, there is a column in the center with incorrect values
+# corrected below ---- 
+
+plastics <- AveRaster_2
+
+r178 <- plastics[cellFromCol(plastics,178)]
+cols <- as.data.frame(r178)
+cols$r179 <- plastics[cellFromCol(plastics,179)]
+cols$r180 <- plastics[cellFromCol(plastics,180)]
+
+cols$r182 <- plastics[cellFromCol(plastics,182)]
+cols$r183 <- plastics[cellFromCol(plastics,183)]
+cols$r184 <- plastics[cellFromCol(plastics,184)]
+
+cols$mean <- rowMeans(cols,na.rm = T)
+
+cols$mean <- ifelse(cols$mean == "NaN",NA,cols$mean)
+cols$mean <- ifelse(is.na(cols$r180) & is.na(cols$r182),NA,cols$mean)
+
+plastics[cellFromCol(plastics,181)] <- cols$mean
+
+plot(plastics)
+
+# Plot difference in coverage between the three models ---- 
+world_sp <- as(world,"Spatial")
+
+VanSeb_01 <- ifelse(VanSeb>0,1,0)
+VanSeb_01 <- ifelse(is.na(VanSeb_01),0,VanSeb_01)
+
+Lebreton_01 <- ifelse(Lebreton>0,1,0)
+Lebreton_01 <- ifelse(is.na(Lebreton_01),0,Lebreton_01)
+
+Maximenko_01 <- ifelse(Maximenko>0,1,0)
+Maximenko_01 <- ifelse(is.na(Maximenko_01),0,Maximenko_01)
+
+sum01 <- VanSeb_01+Lebreton_01+Maximenko_01
+sum01_r <- shift(rotate(raster(sum01, 
+                               xmn = 0, xmx= 360, ymn=-90, ymx=90, 
+                               crs="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")), 
+                 dx=0.5)
+
+sum01_r <- raster :: shift(rotate(raster(sum01, 
+                                         xmn = 0, xmx= 360, ymn=-90, ymx=90, 
+                                         crs="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")), 
+                           by=0.5)
+
+yelblus <- c(brewer.pal(n = 5, name = "YlGnBu"),"#00172e")
+
+cols_nmods <- c("#F2F3F400",yelblus[3:5])
+
+
+plot(sum01_r,col=cols_nmods,breaks = c(-1:3))
+plot(world_sp, col="grey75", add=T)
+
+# Reading in data from refined_datasheet ----
+
+refined_datasheet <- read_sheet("https://docs.google.com/spreadsheets/d/1bVSxqMkHXXxFcxjekMXMv5nfoKfJ5yn6y5UztVNUgJY/edit#gid=0")
+
+medlat = median(refined_datasheet$Latitude)
+medlon = median(refined_datasheet$Longitude)
+
+proj.laea = paste("+proj=laea +lat_0=",round(medlat), " +lon_0=",round(medlon)," +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=km ", sep="")
+spatial_obj <- st_as_sf(refined_datasheet, coords = c("Longitude", "Latitude"), crs = 4326)
+spatial_laea <- st_transform(spatial_obj, crs = crs(proj.laea))
+
+# Setting up a buffer of 250 km for each colony ----
+
+colony_buff <- st_buffer(spatial_laea, dist = 250)
+colony_buff_4326 <- st_transform(colony_buff, crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+plot(plastics)
+plot(colony_buff_4326, col = "blue", add = T) # something's wrong with this graph
+colony_buff_refined <- colony_buff_4326 |> dplyr::select(c('Colony', 'geometry')) 
+
+# Mean plastic abundance value for each colony ----
+result <- vector("list", 11)
+for(i in 1:11){result[[i]] <- mean(values
+                                   (crop
+                                     (plastics, colony_buff_refined[i,2]))
+                                   , na.rm = T)}
+means_vector <- as.vector(unlist(result))
+analysis_df <- cbind(colony_buff_refined, Plastic_debris_mean = means_vector)
+
+# Ordering colony-wise exposure in an intact order ----
+analysis_df$Colony <- factor(analysis_df$Colony, levels = analysis_df$Colony)
+
+# Plotting a basic bar plot for analysis_df ----
+analysis_df_bar_plot <- ggplot(data = analysis_df, aes(x = Colony, y = Plastic_debris_mean)) +
+  geom_col(colour = "black", fill = "skyblue") +
+  scale_y_continuous(name = "Plastic debris mean") +
+  coord_cartesian(ylim = c(1.1,5)) +
+  theme_classic() +
+  theme(axis.line = element_line(colour = "grey"), axis.text.x = element_text(angle = 90)) 
+analysis_df_bar_plot
+
+# Defining a standard deviation and standard error of mean column in analysis_df for error bars in plots ----
+sd_result <- vector("list", 11)
+for(i in 1:11){sd_result[[i]] <- sd(values
+                                    (
+                                      crop
+                                      (plastics, colony_buff_refined[i,2])
+                                    )
+                                    , na.rm = T)}
+sd_vector <- as.vector(unlist(sd_result))
+analysis_df_2 <- cbind(analysis_df, Standard_deviation = sd_vector)
+View(analysis_df_2)
+
+# Defining a sample size column in analysis_df_2 ----
+ss_result <- vector("list", 11)
+for(i in 1:11){
+  ss_result[[i]] <- sum(!is.na
+                        (values(crop(plastics, colony_buff_refined[i,2])))
+  )
+}
+Sample_size <- as.vector(unlist(ss_result))
+analysis_df_2 <- cbind(analysis_df_2, Sample_size)
+View(analysis_df_2)
+transmute_seom <- analysis_df_2 |> transmute(Standard_error_of_mean = Standard_deviation/sqrt(Sample_size))
+analysis_df_2 <- cbind(analysis_df_2, transmute_seom)
+
+
+# Plot with standard deviation ----
+analysis_df_box_plot <- ggplot(data = analysis_df_2, aes(x = Colony, y = Plastic_debris_mean,
+                                                         ymin = Plastic_debris_mean - Standard_deviation,
+                                                         ymax = Plastic_debris_mean + Standard_deviation)) +
+  scale_y_continuous(name = "Plastic debris mean") +
+  geom_boxplot() +
+  theme_classic() +
+  theme(axis.line = element_line(colour = "grey"), axis.text.x = element_text(angle = 90)) +
+  geom_errorbar()
+analysis_df_box_plot
+
+# Plot with seom ----
+analysis_df_seom <- analysis_df_box_plot <- ggplot(data = analysis_df_2, aes(x = Colony, y = Plastic_debris_mean,
+                                                                             ymin = Plastic_debris_mean - Standard_error_of_mean,
+                                                                             ymax = Plastic_debris_mean + Standard_error_of_mean)) +
+  scale_y_continuous(name = "Plastic debris mean") +
+  geom_boxplot() +
+  theme_classic() +
+  theme(axis.line = element_line(colour = "grey"), axis.text.x = element_text(angle = 90)) +
+  geom_errorbar()
+analysis_df_seom
+
+
+# Adding a sample size column in analysis_df ----
+ss_result <- vector("list", 11)
+for(i in 1:11){
+  ss_result[[i]] <- sum(!is.na
+                        (values(crop(plastics, colony_buff_refined[i,2])))
+  )
+}
+Sample_size <- as.vector(unlist(ss_result))
+analysis_df_2 <- cbind(analysis_df, Sample_size)
+View(analysis_df_2)
+
+# Normality testing: ----
+
+# 1. Density plots ----
+library(ggpubr)
+dplot <- vector('list', 11)
+for(i in 1:11){ 
+  dplot[[i]] <- local({
+    i <- i
+    print(ggdensity(na.omit(values(crop(plastics, colony_buff_refined[i,2])))), 
+          main = "Density plot",
+          xlab = "Plastic debris value")})
+}
+library(gridExtra)
+for(j in 1:11){
+  grid.arrange(grobs = dplot, ncol = 5, nrow = 3)
+}
+
+# 2. QQ plots ----
+qplot <- vector('list', 11)
+for(i in 1:11){ 
+  qplot[[i]] <- local({
+    i <- i
+    print(ggqqplot(na.omit(values(crop(plastics, colony_buff_refined[i,2])))), 
+          main = "Density plot",
+          )
+    }
+    )
+}
+library(gridExtra)
+for(j in 1:11){
+  grid.arrange(grobs = qplot, ncol = 5, nrow = 3)
+}
+
+# 3. Shapiro-Wilk's test ----
+shapiro_result <- vector("list", 11)
+for(i in 1:11){
+  shapiro_result[[i]] <- shapiro.test(values(crop(plastics, colony_buff_refined[i,2])))
+}
+for(i in 1:11){print(shapiro_result[[i]])}
+
+# Results:
+# 1: Non-normal
+# 2: Non-normal
+# 3: Non-normal
+# 4: Non-normal
+# 5: Normal
+# 6: Normal
+# 7: Normal
+# 8: Normal
+# 9: Normal
+# 10: Normal
+# 11: Non-normal
+
+# Creating a new analysis data frame with short code
+library(terra)
+values_list <- vector('list', 269)
+for(i in 1:11){
+  values_list[[i]] <- na.omit(values(crop(plastics, colony_buff_refined[i,2])))
+}
+values_vector <- unlist(values_list)
+new_analysis_df <- data.frame(Colony = rep(analysis_df_2$Colony, analysis_df_2$Sample_size),
+                              Plastic_debris_value = values_vector)
+View(new_analysis_df)
+
+# Creating a boxplot for easy visualization ----
+
+boxplot_plastic_debris_values <- ggplot(data = new_analysis_df, aes(x = Colony, y = Plastic_debris_value)) +
+  geom_point() +
+  geom_boxplot() +
+  theme_classic() +
+  scale_y_continuous(name = "Plastic debris value") +
+  theme(axis.line = element_line(colour = "grey"), axis.text.x = element_text(angle = 90)) 
+boxplot_plastic_debris_values
+
+# Playing around with linear models----
+# Checking the assumptions of lm with categorical predictors 
+
+install.packages("performance")
+library(performance)
+install.packages("see")
+library(see)
+trial_lm <- lm(data = new_analysis_df, Plastic_debris_value ~ Colony)
+trial_lm
+summary(trial_lm)
+anova(trial_lm)
+plot(trial_lm)
+check_model(trial_lm)
+shapiro.test(trial_lm$residuals) # Residuals are non-normal
+library(dplyr)
+trimmed_new_analysis_df <- new_analysis_df |> filter(!grepl('Alkefjellet', Colony))
+trimmed_lm <- lm(data = trimmed_new_analysis_df, Plastic_debris_value ~ Colony)
+shapiro.test(trimmed_lm$residuals) # Residuals are non-normal
+
--- My_renditions/nbs_analysis.R
+++ My_renditions/nbs_analysis.R
@@ -1,7 +1,13 @@
+sessionInfo()
 
 # Loading relevant packages
 library(sf)
+library(sp)
+library(raster)
 library(terra)
+library(dplyr)
+library(tidyverse)
+library(adehabitatHR) # trial basis
 
 
 # Working with non-breeding season's data----
@@ -11,38 +17,258 @@ new_data_1 <- readRDS("test_2colonies.rds")
 new_data_2 <- readRDS("test_2colonies_individ_info.rds")
 indiv_merged_df <- merge(new_data_1, new_data_2, by = "individ_id")
 relevant_new_data_1 <- dplyr::select(indiv_merged_df, individ_id, timestamp, lon, lat, loc_type, colony)
+
+# Script- setting an overarching for loop for looping all colonies
+setwd('/Users/ameydanole/Desktop/ENS_Rennes/argh/Microplastic_ingestion_by_fulmarus_glacialis/1_full_analysis_petrels/')
+library(adhabitatHR)
+for(i in unique(relevant_new_data_1$colony)){
+  sub <- relevant_new_data_1 %>% filter(colony == i, !grepl(c('-04-|-05-|-06-|-07-|-08-|-09-') ,timestamp))
+  sub$individ_id <- factor(sub$individ_id, levels = unique(sub$individ_id))
+  sub_sf <- st_as_sf(sub, coords = c("lon", "lat"), crs = 4326)
+  sub_coords <- sub[,c("lon", "lat")]
+  sub_spdf <- SpatialPointsDataFrame(coords = sub_coords, data = sub, proj4string = CRS('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'))
+  sub_crs <-CRS(paste("+proj=laea +lat_0=",median(sub$lat)," +lon_0=",median(sub$lon)," +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=km ", sep = ""))
+  sub_spdf <- spTransform(sub_spdf, CRS = sub_crs)
+  kernel <- kernelUD(sub_spdf[,1])
+  png(paste0('renditions_output/loop_outputs/kernel_image_',i,'.png'), width = 1379, height = 750)
+  plot(unlist(image(kernel)))
+  ud <- getverticeshr(kernel, percent = 95)
+  ud_sf <- st_as_sf(ud)
+  png(paste0('renditions_output/loop_outputs/UD_image_',i,'.png'), width = 1379, height = 750)
+  plot(st_geometry(ud_sf[1,]))
+  dev.off()
+}
+
+# Use ggsave( paste0("map_timing_",m,".png"))
+
+
 sf_relevant <- st_as_sf(relevant_new_data_1[,c(3,4,6)], coords = c("lon", "lat"), crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
-
-# Setting a new crs based on median positions of geolocations----
-
-mean_loc <- geosphere::geomean(cbind(relevant_new_data_1$lon, relevant_new_data_1$lat))
-proj.laea = paste("+proj=laea +lat_0=",mean_loc[2], " +lon_0=",mean_loc[2]," +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=km ", sep="")
-sf_relevant_modified_crs <- st_transform(sf_relevant, crs = crs(proj.laea))
-plot(sf_relevant_modified_crs)
 
 # Come up with season cutoffs and trim data accordingly: use only non-breeding season data
 # For Bjørnøya data:
 
 bjo_nbs_df <- indiv_merged_df |> filter(colony == "Bjørnøya", !grepl(c('-04-|-05-|-06-|-07-|-08-|-09-') ,timestamp)) |> dplyr::select(c(individ_id, timestamp, colony, lon, lat))
 View(bjo_nbs_df)
+bjo_nbs_df[,1] <- as.factor(bjo_nbs_df[,1])
 bjo_sf_nbs_df <- st_as_sf(bjo_nbs_df, coords = c("lon", "lat"), crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
-length(unique(bjo_sf_nbs_df$individ_id))
+length(unique(bjo_sf_nbs_df$individ_id)) # 38 unique individuals
+bjo_values <- raster::extract(plastics, bjo_sf_nbs_df, df = T)
+
+# For Jan Mayen data: non-breeding season is defined here from April to September
+
+jan_nbs_df <- indiv_merged_df |> filter(colony == "Jan Mayen", !grepl(c('-04-|-05-|-06-|-07-|-08-|-09-0') ,timestamp)) |> dplyr::select(c(individ_id, timestamp, colony, lon, lat))
+jan_nbs_df[,1] <- as.factor(jan_nbs_df[,1])
+jan_sf_nbs_df <- st_as_sf(jan_nbs_df, coords = c("lon", "lat"), crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+length(unique(jan_sf_nbs_df$individ_id)) # 48 unique individuals
+
+# Trying adehabitatHR for home range estimation: kernel density estimation was used in the 2023
+# petrels paper so might be interesting to dig in more
+
+# Checking out the adehabitat package----
+
+install.packages("adehabitatHR")
+library(adehabitatHR)
+
+bjo_ade <- bjo_nbs_df |> dplyr::select(c('lon', 'lat'))
+bjo_ade_matrix <- as.matrix(bjo_ade, ncol = 2)
+bjo_ade_sp <- SpatialPoints(bjo_ade_matrix)
+cp <- mcp(bjo_ade_sp)
+as.data.frame(cp)
+
+# Estimating home range for each individual using minimum convex polygon (MCP)----
+# Creating a SpatialPointsDataFrame 
+bjo_coords <- bjo_nbs_df[,c("lon","lat")]
+jan_coords <- jan_nbs_df[,c("lon","lat")]
+
+# Assigning bjo's geomedian-based crs to bjo_spdf
+
+bjo_spdf <- SpatialPointsDataFrame(coords = bjo_coords, data = bjo_nbs_df, proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+bjo_crs <-CRS("+proj=laea +lat_0=73.42981 +lon_0=20.8018574294084 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=km ")
+bjo_spdf_1 <- spTransform(bjo_spdf, bjo_crs)
+bjo_cp <- mcp(bjo_spdf_1[,1])
+as.data.frame(bjo_cp)
+plot(bjo_cp)
+plot(bjo_spdf_1, col=as.data.frame(bjo_spdf_1)[,1], add = T) 
+bjo_cp
+diff_lev <- mcp.area(bjo_spdf_1[,1]) # home range across different levels
+kernel <- kernelUD(bjo_spdf_1[,1]) # finally, kernel densities!
+image(kernel)
+head(kernel)
+image(kernel[[1]])
+ud <- getverticeshr(kernel, percent = 90)
+ud_1 <- st_as_sf(ud)
+plot(st_geometry(ud_1[1,]))
+# messed up for now, will find out how to segregate each of them 38 polygons
+
+# Okay for subsetting unique individuals use- set a loop to get kernel densities for each individual
+# First create a list of all unique individuals
+bjo_unique_ind <- unique(bjo_spdf$individ_id)
+bjo_spdf_df <- as.data.frame(bjo_spdf)
+grouped_bjo <- bjo_spdf_df |> group_by(individ_id) |> summarize(n = n())
+View(grouped_bjo)
+
+output_df <- list()
+bjo_spdf_df <- as.data.frame(bjo_spdf)
+for(i in 1:length(bjo_unique_ind)){
+  output_df[[i]] <- bjo_spdf_df |> filter(individ_id == bjo_unique_ind[i])
+}
+output_df
+
+# I'd first have to convert output_df into SpatialPointsDataFrames
+spatial_output_df <- list()
+bjo_spatial_coords <- list()
+for(i in 1:length(bjo_unique_ind)){
+  bjo_spatial_coords[[i]] <- output_df[[i]][,c("lon","lat")]
+  spatial_output_df[[i]] <- SpatialPointsDataFrame(coords = bjo_spatial_coords[[i]], data = output_df[[i]], proj4string = bjo_crs)
+}
+
+# Setting up another for loop for calculating 95% UDs
+x <- seq(-75, 175, by=1.) # resolution is the pixel size you desire 
+y <- seq(0, 100, by=1.)
+xy <- expand.grid(x=x,y=y)
+coordinates(xy) <- ~x+y
+gridded(xy) <- TRUE
+class(xy)
+
+bjo_kernel <- vector("list", 38)
+bjo_ud <- list("list", 38)
+for(i in 1:length(bjo_unique_ind)){
+  bjo_kernel[[i]] <- kernelUD(spatial_output_df[[i]][,3], grid = xy)
+}
+
+  for (i in 1:length(bjo_unique_ind)){
+    bjo_ud[[i]] <- getverticeshr(bjo_kernel[[i]], percent = 95)
+  }
+
+NOS-4181262
+subset_1 <- subset(bjo_spdf, individ_id == "NOS-4181262")
+unique(bjo_spdf$individ_id)
+head(subset_1)
+subset_1_kernel <- kernelUD(subset_1[,3])
+image(subset_1_kernel)
+subset_1_ud <- getverticeshr(subset_1_kernel, percent = 90)
+plot(subset_1_ud, add = T)
+
+# # trying out stuff from documentation
+# volume <- getvolumeUD(subset_1_kernel)
+# levels <- c(50, 75, 90)
+# listt <- vector(mode = "list", length = 2)
+# listt[[1]] <- as.image.SpatialGridDataFrame(volume[[1]])
+# contour(list[[1]], levels = levels)
+
+head(subset_1_ud)
+library(sf)
+fort <- fortify(subset_1_ud)
+head(fort)
+ggplot(fort, aes(x = long, y = lat)) + geom_polygon(alpha = 0.4) + coord_equal()
+
+# ggtiles as the plastic raster
+library(ggplot2)
+plastics_spdf <- as(plastics, "SpatialPixelsDataFrame")
+plastics_spdf_df <- as.data.frame(plastics_spdf)
+colnames(plastics_spdf_df) <- c("Value", "x", "y")
+
+bjo_kernel_overlap <- ggplot(fort, aes(x = long, y = lat)) +  
+  coord_equal() +
+  geom_tile(data = plastics_spdf_df, aes(x = x, y = y, fill = Value), alpha = 0.8) + 
+  scale_fill_viridis_c() +
+  geom_polygon(alpha = 0.6) +
+  # theme_classic() +
+  theme(legend.position="bottom", legend.key.width=unit(2, "cm"), axis.line = element_line("grey"),
+        plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5)) +
+  labs(fill = "Floating plastic debris value",  
+       title = "Bjørnoya colony", 
+       subtitle = "95% utilization distributions of northern fulmars from Bjørnoya colony during their non-breeding season",
+       tag = "A")
+bjo_kernel_overlap
+
+# Trials
+install.packages("amt")
+library(amt)
+library(units) # to show spatial units in ggplots
+library(MetBrewer) # to display colour palettes
+install.packages("rnaturalearth")
+library(rnaturalearth)
+install.packages("aniMotum")
+
+# Wrangling and preparing data for KDE
+dat.track <- make_track(bjo_nbs_df, lon, lat, crs = "+proj=laea +lat_0=73.42981 +lon_0=20.8018574294084 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=km")
+# Template raster for KDE to map on to
+trast <- make_trast(dat.track, res = 10) 
+trast
+# Calculating kde for individual ids
+dat.kde.ref <- hr_kde(dat.track, trast = trast, h = hr_kde_ref(dat.track), levels = c(0.5,0.95))
+dat.kde.ref
+plot(dat.kde.ref, col = c("red", "blue"))
+kde.href.contours <- hr_isopleths(dat.kde.ref)
+
+# ggplot() + geom_tile(data = raster::as.data.frame(dat.kde.ref$ud)) +
+#   scale_fill_viridis_c(option = "inferno") +
+#   theme_bw()
+
+
+
+NOS-4181268
+subset_2 <- subset(bjo_spdf, individ_id == "NOS-4181268")
+subset_2_kernel <- kernelUD(subset_2[,3])
+image(subset_2_kernel)
+subset_2_ud <- getverticeshr(subset_2_kernel, percent = 90)
+plot(subset_2_ud, add = T)
+
+NOS-5109523
+subset_38 <- subset(bjo_spdf, individ_id == "NOS-5109523")
+subset_38_kernel <- kernelUD(subset_38[,3])
+image(subset_38_kernel)
+subset_38_ud <- getverticeshr(subset_38_kernel, percent = 95)
+plot(subset_38_ud, add = T)
+
+bjo_unique_ind
+
+
+jan_coords <- jan_nbs_df[,c("lon","lat")]
+jan_spdf <- SpatialPointsDataFrame(coords = jan_coords, data = jan_nbs_df)
+jan_cp <- mcp(jan_spdf[,1])
+as.data.frame(jan_cp)
+plot(jan_cp)
+
+clu <- clusthr(bjo_ade_sp) # takes too long to execute
+class(clu)
+ade_df <- bjo_nbs_df[,-3] 
+ade_spdf <- SpatialPointsDataFrame(coords = c(ade_df$lon, ade_df$lat), data = ade_df)
+cp <- mcp(ade_df[,3])
+?SpatialPointsDataFrame
+
+# Setting a new crs based on median positions of geolocations---- 
+# no utility as of now
+
+# For Bjørnoya 
+bjo_medlat = median(bjo_nbs_df$lat)
+bjo_medlon = median(bjo_nbs_df$lon)
+bjo_proj.laea = paste("+proj=laea +lat_0=",bjo_medlat, " +lon_0=",bjo_medlon," +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=km ", sep="")
+bjo_mod <- st_transform(bjo_sf_nbs_df, crs = crs(bjo_proj.laea))
+plot(bjo_mod$geometry)
+
+# For Jan Mayen
+jan_medlat = median(jan_nbs_df$lat)
+jan_medlon = median(jan_nbs_df$lon)
+jan_proj.laea = paste("+proj=laea +lat_0=",jan_medlat, " +lon_0=",jan_medlon," +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=km ", sep="")
+jan_mod <- st_transform(jan_sf_nbs_df, crs = crs(jan_proj.laea))
+plot(jan_mod$geometry)
+
+# Plotting graphs for clarity 
+
+# Bjørnoya
 plot(plastics)
 plot(bjo_sf_nbs_df, cex = 0.4, col = "blue", pch = 16, add = T)
 plot(crop(plastics, bjo_sf_nbs_df))
 plot(bjo_sf_nbs_df, cex = 0.4, col = "blue", pch = 16, add = T)
 
-# For Jan Mayen data: non-breeding season is defined here from April to September
-
-jan_nbs_df <- indiv_merged_df |> filter(colony == "Jan Mayen", !grepl(c('-04-|-05-|-06-|-07-|-08-|-09-0') ,timestamp)) |> dplyr::select(c(individ_id, timestamp, colony, lon, lat))
-jan_sf_nbs_df <- st_as_sf(jan_nbs_df, coords = c("lon", "lat"), crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+# Jan Mayen
 plot(plastics)
 plot(jan_sf_nbs_df, cex = 0.4, col = "blue", pch = 16, add = T)
 jan_nbs_cropped <- crop(plastics, jan_sf_nbs_df)
 extent(jan_sf_nbs_df)
 plot(jan_nbs_cropped, cex = 0.4, pch = 16)
-jan_values_trial <- raster::extract(plastics, jan_sf_nbs_df, df = T)
-length(unique(jan_sf_nbs_df$individ_id))
 
 # To find: how to crop just vector point corresponding data from a raster
 
@@ -144,13 +370,4 @@ jan_nbs_values <- raster::extract(x = plastics, y = jan_sf_nbs_df, df = T)
 # Read phenology papers for stringent seasonal bounds; check petrels paper for what kind of analysis 
 # they've done; what different analyses we could run, do plots like the ones in the fisheries paper where red and blue display temporal trends. 
 
-# Checking out the adehabitat package in R
 
-install.packages("adehabitatHR")
-library(adehabitatHR)
-
-trial_no <- relevant_new_data_1 |> dplyr::select(c('lon', 'lat'))
-trial_no_matrix <- as.matrix(trial_no, ncol = 2)
-trial_no_sp <- SpatialPoints(trial_no_matrix)
-clu<- clusthr(trial_no_sp)
-class(clu)
