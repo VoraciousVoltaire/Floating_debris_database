e8129f95722bff57d23690ce89ff1116d8d27b97
Azure._.Damsel <136301398+VoraciousVoltaire@users.noreply.github.com>
2024-01-22 17:15:14 +0100

    Delete Basics_chap1.R

--- Basics_chap1.R
+++
@@ -1,7 +0,0 @@
-data(iris)
-plot(iris[,1:4], pch = 1, cex = 0.7, col = "grey30", upper.panel = panel.smooth)
-sapply(iris, class)
-as.matrix(iris[1:10,-5])
-iris$Species[10] = "bananas"
-attributes(iris$Species)
-levels(iris$Species)
